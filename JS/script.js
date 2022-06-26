69912a9b3f189209cdb1e34e515f31e07400e4ac
RomuloRatis <entreratoseleoes@gmail.com>
2022-06-26 16:35:57 -0300

    Estrutura de dados complexos

---
+++ JS/script.js
@@ -0,0 +1,19 @@
+let lista = ["bolo", "café", "biscoito", "pão", "suco"];
+    console.log(lista);
+
+lista.unshift("Vitamina");
+    console.log(lista);
+
+lista.pop();
+    console.log(lista);
+
+lista.push("Torta", "Sanduíche");
+    console.log(lista);
+
+lista.shift();
+    console.log(lista);
+
+let numbers = [7, 5, 6, 3, 8, 9, 2, 1, 4];
+    
+numbers.sort();
+    console.log(numbers);
